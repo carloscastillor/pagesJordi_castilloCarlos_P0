f4f03cf6b76326d853db65e48cd6c32a19888cf8
Carlos <ccastillor@edu.tecnocampus.cat>
2021-01-31 19:35:08 +0100

    Punto 5

--- pagesJordi_castilloCarlos_P0/main/power_up.go
+++ pagesJordi_castilloCarlos_P0/main/power_up.go
@@ -1,3 +1,18 @@
+components {
+  id: "power_up"
+  component: "/Scripts/power_up.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
